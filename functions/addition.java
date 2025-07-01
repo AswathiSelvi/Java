45d0cfa310390f9ab73f0c4f445de530f07fb62d
AswathiSelvi <aswathiselvi2@gmail.com>
2025-07-01 11:21:51 +0530

    practice program

---
+++ functions/addition.java
@@ -0,0 +1,11 @@
+public class addition{
+    int a=10;
+    int b=20;
+    void sum(){
+        System.out.println("the sum is:"+(a+b));
+    }
+    public static void main (String [] args){
+        addition num1=new addition();
+        num1.sum();
+    }
+}
---
+++ functions/mobile.java
@@ -0,0 +1,20 @@
+public class mobile{
+    
+        void iphone(){
+            System.out.println("This is IPHONE.");
+        }
+        void samsung(){
+            System.out.println("This is SAMSUNG");
+        }
+        void oppo(){
+            System.out.println("This is OPPO");
+        }
+        void realme(){
+            System.out.println("This is REALME");
+        }
+        public static void main(String[] args) {
+            mobile obj1=new mobile();
+            obj1.iphone();
+
+    }
+}
